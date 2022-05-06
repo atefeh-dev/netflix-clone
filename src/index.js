f50b9b71aeb7300591a76e5e92ec8a2b14d45556
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-06 20:46:41 +0430

    a

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ import App from "./components/App";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./store/reducers";
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/https://74b1d70e-e23b-4a68-98f9-4eca94bef9e0-europe-west1.apps.astra.datastax.com/api/graphql/netflix",
+  cache: new InMemoryCache(),
+});
+
+const Main = () => (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
+  </BrowserRouter>
 );
+
+ReactDOM.render(<Main />, document.querySelector("#root"));

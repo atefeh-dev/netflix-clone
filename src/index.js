83e6deabf611a75d4ff90abe2a5a5209791d33d1
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-07 14:47:05 +0430

    deleted:    node_modules/.abab-THkHctdU/CHANGELOG.md 	deleted:    node_modules/.abab-THkHctdU/LICENSE.md 	deleted:    node_modules/.abab-THkHctdU/README.md 	deleted:    node_modules/.abab-THkHctdU/index.d.ts 	deleted:    node_modules/.abab-THkHctdU/index.js 	deleted:    node_modules/.abab-THkHctdU/lib/atob.js 	deleted:    node_modules/.abab-THkHctdU/lib/btoa.js 	deleted:    node_modules/.abab-THkHctdU/package.json 	deleted:    node_modules/.acorn-hsUF03q0/CHANGELOG.md

--- src/index.js
+++ src/index.js
@@ -9,10 +9,28 @@ import thunk from "redux-thunk";
 import reducers from "./store/reducers";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const customFetch = (uri, options) => {
+  return fetch(uri, {
+    ...options,
+    headers: {
+      ...options.headers,
+      "x-custom-header": process.env.ASTRA_DB_APPLICATION_TOKEN,
+    },
+  });
+};
+
+const fetchLink = createHttpLink({
+  uri: process.env.ASTRA_GRAPHQL_ENDPOINT,
+  fetch: customFetch,
+});
+
 const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/https://74b1d70e-e23b-4a68-98f9-4eca94bef9e0-europe-west1.apps.astra.datastax.com/api/graphql/netflix",
+  link: ApolloLink.from([fetchLink]),
   cache: new InMemoryCache(),
 });
 

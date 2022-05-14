62a26ec106ec9921774585a4d290804c49af3e99
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-14 18:52:47 +0430

    fetch more genre

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 /** @format */
 
 import Section from "./Section";
-import React from "react";
+import React, { useState } from "react";
 import { useGenre } from "../hook/useGenre";
 
 const App = () => {
   const incrementLimit = 4;
-  const { error, loading, data } = useGenre(incrementLimit);
+  const [limit, setLimit] = useState(incrementLimit);
+  const { error, loading, data } = useGenre(limit);
   if (loading) return <div>spinner...</div>;
   if (error) return <div> something is wrong ...</div>;
   else {
+    console.log(limit);
     return (
       <div className="Container">
         {data.genre_list.values.map((genre, index) => {
           return <Section genre={genre.value} key={index} />;
         })}
+        <div
+          className="end-section"
+          onMouseEnter={() => {
+            setLimit(limit + incrementLimit);
+          }}
+        />
       </div>
     );
   }
